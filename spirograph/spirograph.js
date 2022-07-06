461eb5b0286c1e0b9dd200caf98e6e4f3da80565
Hitesh suthar <86836656+Hitesh-suthar@users.noreply.github.com>
2022-07-06 19:34:21 +0530

    Update spirograph.js

--- spirograph/spirograph.js
+++ spirograph/spirograph.js
@@ -1,86 +1,93 @@
-let spirals = [];
-let speed = 0.12;
-let n = 1;
-let i = 0;
-let drawing = true;
-let backCol;
-function setup(){
-    createCanvas(windowWidth,(windowHeight-140));
+let speed = document.getElementById('speed').value;
+let spiral;
+
+function setup() {
+    const width = document.body.clientWidth - 15;
+    const height = width < windowHeight ? width : windowHeight;
+    createCanvas(width, height);
     colorMode(HSB);
-    backCol = color(0,1,95);
-    for(let i = 0 ; i < n ; i++){
-        spirals[i] = new spirograph(speed);      
+    spiral = new spirograph(speed);
+}
+
+function draw() {
+    background(0, 0, 0);
+    translate(width / 2, height / 2);
+    if (spiral) {
+        spiral.construct();
+        spiral.show();
+    }
+    else {
+        fill(0, 0, 255, 0.5);
+        textAlign(CENTER);
+        textSize(34);
+        text("Click on new spiral", 0, 0)
     }
 }
-function draw (){
-        background(backCol);
-        for(let j = 0 ; j < spirals.length ; j++){
-            if(drawing){
-                for(let k=0;k<10;k++){
-                    spirals[j].construct();
-                    i++;
-                }
-            }
-            spirals[j].show();
-        }
-        if(i>5000) {
-            drawing = false;
-            noLoop();
-        }
+
+function newSpiral() {
+    speed = document.getElementById('speed').value;
+    spiral = new spirograph(speed);
+    loop();
 }
+
+function download() {
+    saveCanvas('myCanvas', 'png');
+}
+
+function reset() {
+    spiral = null;
+    noLoop();
+}
+
 class spirograph {
 
-    constructor( speed , R = (width < height ? width/2 : height/2) ,  l = random(0,1), k = random(0,1) ){
+    constructor(speed , R = (width < height ? width / 2 : height / 2), l = random(0, 1), k = random(0, 1)) {
         this.R = R;
         this.l = l;
         this.k = k;
+        this.step = 0.1;
         this.speed = speed;
         this.t = 0;
+        this.isdrawing = true;
         this.points = [];
-        this.color = color( random(255) , random(50,255) , random(0,100) );                    
+        this.color = color(random(255), random(255), random(200, 255));
     }
 
     construct() {
-        this.t = this.t - this.speed;
-        const P = {
-            x : this.R*((1-this.k)*Math.cos(this.t) + this.l*this.k*Math.cos(((1-this.k)*this.t)/this.k)),
-            y : this.R*((1-this.k)*Math.sin(this.t) - this.l*this.k*Math.sin(((1-this.k)*this.t)/this.k))
-        };
-        this.points.push(P);
+        if (this.isdrawing) {
+            for(let i=0;i<this.speed; i++){
+                const P = {
+                x: this.R * ((1 - this.k) * Math.cos(this.t) + this.l * this.k * Math.cos(((1 - this.k) * this.t) / this.k)),
+                y: this.R * ((1 - this.k) * Math.sin(this.t) - this.l * this.k * Math.sin(((1 - this.k) * this.t) / this.k))
+                };
+                this.t = this.t - this.step;
+                this.points.push(P);
+            }
+        }
+        if (this.points.length > 6000) this.isdrawing = false;
     }
 
-    show(){
+    show() {
         push();
-        translate(width/2,height/2);
         beginShape();
         stroke(this.color);
         noFill();
-        for( let p of this.points ){
-            vertex(p.x , p.y);   
+        for (let p of this.points) {
+            vertex(p.x, p.y);
         }
         endShape();
         pop();
     }
+
+    play() {
+        this.isdrawing = true;
+    }
+    pause() {
+        this.isdrawing = false;
+    }
 }
-function play(){
-    drawing = true;
-}
-function pause(){
-    drawing = false;
-}
-function reset(){
-    spirals = [];
-    drawing = false;
-    loop();
-}
-function download(){
-    saveCanvas('myCanvas', 'png');
-}
-function newS(){
-    spirals.pop();
-    i=0;
-    let temp = new spirograph(speed);
-    spirals.push(temp);
-    drawing = true;
-    loop();
+
+function handlechange(e){
+    document.getElementById('speedvalue').innerHTML = e.value;
+    spiral.speed = e.value;
 }
